51f5a52e82eb8e87538fa64be5dbaae5bd226415
Yunkyungii <uk000509@gmail.com>
2023-08-17 12:41:30 +0900

    깔끔이 그누보드 실습

---
+++ basic/sub_head.php
@@ -0,0 +1,29 @@
+<?php
+if($board['bo_table'] == 'qa') {
+    $page_title = '문의게시판';
+    $cate_title = '고객지원';
+    $page_num = 1;
+    $cate_num = 6;
+}
+
+if($board['bo_table'] == 'gallery') {
+    $page_title = '청소갤러리';
+    $cate_title = '고객지원';
+    $page_num = 1;
+    $cate_num = 5;
+}
+
+if($board['bo_table'] == 'notice') {
+    $page_title = '공지사항';
+    $cate_title = '고객지원';
+    $page_num = 1;
+    $cate_num = 7;
+}
+?>
+
+<main id="sub" class="sub" role="main">
+      <article class="subpage">
+          <div class="inner">
+          <div class="page">
+                    <strong><?=$page_title;?></strong> / <span><?=$cate_title;?></span>
+                </div>
